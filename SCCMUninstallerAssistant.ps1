b0ce4c6f67e92c75170f14777c8f61337814dc85
Matt Allbright <allbrigh@email.arizona.edu>
2023-02-23 16:13:19 -0700

    Create SCCMUninstallerAssistant.ps1

---
+++ SCCMUninstallerAssistant.ps1
@@ -0,0 +1,45 @@
+function Write-HostCenter { 
+param($Message) Write-Host ("{0}{1}" -f (' ' * (([Math]::Max(0, $Host.UI.RawUI.BufferSize.Width / 2) - [Math]::Floor($Message.Length / 2)))), $Message) 
+}
+
+cls
+
+Write-HostCenter "UninstallerAssistant`n"
+Write-HostCenter "Matthew Allbright // version 2022.11.30`n"
+
+Write-HostCenter "This script can be used to quickly send commands to"
+Write-HostCenter "networked computers to uninstall applications deployed"
+Write-HostCenter "in the Microsoft Endpoint Software Center (SCCM)"
+Write-HostCenter "system.`n"
+
+# Stores name input
+$computer = Read-Host "What computer are you working on?"
+
+echo "Below is a list of programs to uninstall."
+$applicationsAvailable = $(Get-WmiObject -Namespace "root\ccm\clientSDK" -Class CCM_Application -ComputerName $computer | Where-Object { $_.EvaluationState -like "1" } | ForEach-Object { $_.Name })
+echo "`n"
+echo "---------------"
+echo "`n"
+foreach ($app in $applicationsAvailable) {
+	echo $app
+}
+echo "What program would you like to uninstall?"
+echo "`n"
+echo "---------------"
+echo "`n"
+$applicationName = Read-Host "Type a name or partial name"
+		
+$application = (Get-WmiObject -ClassName CCM_Application -Namespace "root\ccm\clientSDK" -ComputerName $computer | Where-Object {$_.Name -like "*$applicationName*" -and $_.EvaluationState -like "1"})
+ 
+if ($application.IsMachineTarget -is [array]) {
+    echo "Please be more specific with the software name, your current selection returns too many results."
+} else {
+    #$applicationArgs = @{EnforcePreference = [UINT32] 0
+    #Id = "$($application.id)"
+    #IsMachineTarget = $application.IsMachineTarget
+    #IsRebootIfNeeded = $False
+    #Priority = 'High'
+    #Revision = "$($application.Revision)" }
+		
+    Invoke-WmiMethod -Namespace "root\ccm\clientSDK" -Class CCM_Application -ComputerName $computer -Name Uninstall -ArgumentList @(0,$application.ID,$application.IsMachineTarget,$false,'High',$application.Revision)
+}
